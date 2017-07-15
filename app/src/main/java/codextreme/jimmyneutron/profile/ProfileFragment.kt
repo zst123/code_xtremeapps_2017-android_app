389a4165b0d11e72063116cfa111ceeecf879690
Manzel Seet <manzel.seet@gmail.com>
2017-07-15 09:27:09 +0800

    Add partial bargraph

--- app/src/main/java/codextreme/jimmyneutron/profile/ProfileFragment.kt
+++ app/src/main/java/codextreme/jimmyneutron/profile/ProfileFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import codextreme.jimmyneutron.Common
 import codextreme.jimmyneutron.R
+import im.dacer.androidcharts.BarView
 
 
 public class ProfileFragment : Fragment() {
@@ -17,6 +18,12 @@ public class ProfileFragment : Fragment() {
         val view = inflater?.inflate(R.layout.fragment_profile, container, false)
         val userprofile_name = view?.findViewById(R.id.userprofile_name) as TextView
         userprofile_name.text = Common.mUsername
+
+        val barView = view?.findViewById(R.id.bar_view) as BarView
+        barView.setBottomTextList(arrayListOf("S", "M", "T", "W", "T", "F", "S"))
+        val dataList = arrayListOf(10, 50, 70, 30, 60, 20, 80)
+        barView.setDataList(dataList, 100)
+
         return view
     }
 }
